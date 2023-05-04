f01c07428a76aadf12eb1d8ba2ded443f7c245e6
Aman Kumar <amanmandal644@gmail.com>
2023-05-05 04:26:59 +0530

    Added all things in structured form

--- Backend/index.js
+++ Backend/index.js
@@ -1,40 +1,47 @@
-const express= require('express');
-const app= express();
+const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const app = express();
 
 
-
-require('dotenv').config();
-
-
-const {connection}= require('./configs/db');
-const { logsData } = require('./middlewares/log.middleware');
-const {authenicate}= require('./middlewares/authentication')
-const {userRouter}= require('./routes/user.route');
-const { adminRouter } = require('./routes/admin.router');
+require("dotenv").config();
 
 
+// --------------->>>>>>>> Location of Routes & Middlewares <<<<<<<<-------------------
+const { dbconnetion } = require("./configs/db");
+const { logsData } = require("./middlewares/log.middleware");
+const { authentication } = require("./middlewares/authentication");
+const { userRouter } = require("./routes/user.route");
+const { adminRouter } = require("./routes/admin.router");
 
+
+// --------------->>>>>>>> Middlewares <<<<<<<<-------------------
+app.use(cors());
+app.use(cookieParser());
 app.use(express.json());
 
 
+// --------------->>>>>>>> Default End Point <<<<<<<<-------------------
+app.get("/", (req, res) =>
+  res.send(
+    `<h1 style="text-align:Center;color:blue">Welcome in Snips & Spikes API</h1>`
+  )
+);
 
 
+// --------------->>>>>>>> Routers <<<<<<<<-------------------
 app.use(logsData);
 app.use("/users", userRouter);
 app.use("/admin", adminRouter);
 
 
-
-
-app.listen(8080, async()=>{
- try {
-    await connection;
-    console.log("Connected to DB");
- } catch (error) {
-    console.log("Error While Connected to DB");
-    console.log(error);
- }
- console.log("Runnig on YOUr Port");
-})
+// --------------->>>>>>>> Server Running <<<<<<<<-------------------
+app.listen(process.env.PORT, async () => {
+  try {
+    await dbconnetion;
+    console.log(`Connected to Database`);
+    console.log(`Server listening on ${process.env.PORT}`);
+  } catch (error) {
+    console.log(`Error while connecting to ${error.message}`);
+  }
+});
