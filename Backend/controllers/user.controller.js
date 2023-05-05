3d478e2fb8063134dfa2c55c1ce95701e6ee4065
Aman Kumar <amanmandal644@gmail.com>
2023-05-06 02:43:33 +0530

    User Controllers Added

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,10 +1,10 @@
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const jwt = require ("jsonwebtoken");
+const bcrypt = require ("bcrypt");
 
 require("dotenv").config();
 
-
-const { UserModel } = require("../models/user.model");
+const { UserModel } = require ("../models/user.model");
+const { sendEmail } = require("../services/mail");
 
 
 const signup = async (req, res) => {
@@ -15,7 +15,7 @@ const signup = async (req, res) => {
         if (isPresent.length === 0) {
             // encrypte password and register
             bcrypt.hash(password, 5, async (err, hash) => {
-                if (err) res.status(401).json({ error: err.message });
+                if (err) res.status(401).json({ "error": err.message });
                 else {
                     const newUser = new UserModel({
                         name,
@@ -23,96 +23,89 @@ const signup = async (req, res) => {
                         password: hash,
                     });
                     await newUser.save();
-                    res.status(200).json({
-                        success: "user registered successfully",
-                    });
+                    res.status(200).json({ success: "user registered successfully" });
                 }
             });
         } else {
-            res.status(404).json({ error: "user already registered" });
+            res.status(404).json({ msg: "user already registered" });
         }
     } catch (error) {
-        res.status(500).json({ error: error.message });
+        res.status(500).json({ msg: error.message });
     }
-};
-
+}
 
 const login = async (req, res) => {
     try {
         const { email, password } = req.body;
         const UserData = await UserModel.findOne({ email });
-
+        
         if (!UserData) {
-            return res.status(404).json({ error: "user not found" });
+            res.status(404).json({ message: "user not found" });
         }
+
+        // hash password form UserData(db.users)
         const hashPassword = UserData?.password;
+        
+        // compare  
         bcrypt.compare(password, hashPassword, (err, result) => {
             if (result) {
-                const normalToken = jwt.sign(
-                    { userId: UserData._id },
-                    process.env.NORMALKEY,
-                    { expiresIn: "7d" }
-                );
-                const refreshToken = jwt.sign(
-                    { userId: UserData._id },
-                    process.env.REFRESHKEY,
-                    { expiresIn: "7d" }
-                );
+                // send otp
+                const otp = Math.round((Math.random() * 9999))
+                console.log(otp);
+                sendEmail({email: email,subject:"Login OTP",body:`OTP is ${otp}` })
+                
+                // generate tokens 
+                const Normal_Token = jwt.sign({ userId: UserData._id }, process.env.NORMALKEY, { expiresIn: "7d" })
+                const Refresh_Token = jwt.sign({ userId: UserData._id }, process.env.REFRESHKEY, { expiresIn: "28d" })
 
-                res.cookie("normalToken", normalToken, { httpOnly: true });
-                res.cookie("refreshToken", refreshToken, { httpOnly: true });
-                res.status(200).json({
-                    message: "Login successfully",
-                    normalToken,
-                    refreshToken,
-                    name: UserData["name"],
-                    email,
-                    userid: UserData["_id"],
-                });
-            } else {
-                res.status(401).json({ error: "error while login" });
+                // send token in cookies
+                res.cookie("Normal_Token", Normal_Token, { httpOnly: true })
+                res.cookie("Refresh_Token", Refresh_Token, { httpOnly: true })
+                res.status(200).json({ "message": "Login successfully", Normal_Token, Refresh_Token,name:UserData["name"],email,userid:UserData["_id"],otp: otp});
             }
-        });
+            else {
+                res.status(401).json({ "message": "error while login" });
+            }
+        })
     } catch (error) {
-        res.status(404).json({ error: error.message });
+        res.status(404).json({ message: error.message });
     }
-};
+ 
+}
 
-
-const allUsers = async (req, res) => {
+const getalluser = async (req, res) => {
     try {
-        if (req.body.access_key === process.env.ACCESSKEY) {
+        if (req.body.access_key === process.env.ACCESSKEY ) {
+
             const UserData = await UserModel.find();
             res.status(200).json({ UserData });
-        } else {
-            res.status(401).json({ error: "Access denied" });
         }
-    } catch (error) {
-        res.status(400).json({ error: error.message });
+        else {
+            res.status(401).json({ message: "Access denied" });
+        }
     }
-};
-
+    catch (error) {
+        res.status(400).json({ message: error.message });
+        
+    }
+}
 
 const getUser = async (req, res) => {
-    const _id = req.params.id;
-    try {
-        if (req.body.access_key === process.env.ACCESSKEY) {
-            const UserData = await UserModel.findOne({ _id });
-            res.status(200).json({ UserData });
-        } else {
-            res.status(401).json({ error: "Access denied" });
+        const _id = req.params.id;
+        try {
+            if (req.body.access_key === process.env.ACCESSKEY ) {
+                const UserData = await UserModel.findOne({_id});
+                res.status(200).json({ UserData });
+            }
+            else {
+                res.status(401).json({ message: "Access denied" });
+            }
+            
         }
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
-};
-//  const logout=(req,res)=>{
-//     const token= req.headers.authorization;
-//     const blackData= JSON.parse(fs.readFileSync("./blacklist.json", "utf-8"));
-//     blackData.push(token);
-//     fs.writeFileSync("../", JSON.stringify(blackData));
-//     console.log(blackData);
-//     res.send("LogOut Succesfully")
-// }
+        catch (error) {
+            res.status(400).json({ message: error.message });
+            
+        }
+}
 
-module.exports = { signup, login, allUsers, getUser };
+module.exports = {signup ,login ,getalluser ,getUser}
