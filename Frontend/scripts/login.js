b113035c553ad8a98b2deabf98610d4a8fe78d75
Tina Rawat <tinarawat03@gmail.com>
2023-05-09 01:01:41 +0530

    unnecessary console removed from frontend

--- Frontend/scripts/login.js
+++ Frontend/scripts/login.js
@@ -4,161 +4,149 @@ const container = document.getElementById("container");
 const baseurl = "http://localhost:8080/";
 
 signUpButton.addEventListener("click", () => {
-    container.classList.add("right-panel-active");
+  container.classList.add("right-panel-active");
 });
 
 signInButton.addEventListener("click", () => {
-    container.classList.remove("right-panel-active");
+  container.classList.remove("right-panel-active");
 });
 
 const Toast = Swal.mixin({
-    toast: true,
-    position: "top-end",
-    showConfirmButton: false,
-    timer: 2000,
+  toast: true,
+  position: "top-end",
+  showConfirmButton: false,
+  timer: 2000,
 });
 Toast.fire({
-    icon: "success",
-    title: "Welcome to Login page",
+  icon: "success",
+  title: "Welcome to Login page",
 });
 
 // signup
 
 const singupfrom = document.getElementById("signUpForm");
 singupfrom.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const signUpName = document.getElementById("signUpName");
-    const signUpEmail = document.getElementById("signUpEmail");
-    const signUpPassword = document.getElementById("signUpPassword");
-    const userDetails = {
-        name: signUpName.value,
-        email: signUpEmail.value,
-        password: signUpPassword.value,
-    };
-    register(userDetails);
-    console.log(userDetails);
+  e.preventDefault();
+  const signUpName = document.getElementById("signUpName");
+  const signUpEmail = document.getElementById("signUpEmail");
+  const signUpPassword = document.getElementById("signUpPassword");
+  const userDetails = {
+    name: signUpName.value,
+    email: signUpEmail.value,
+    password: signUpPassword.value,
+  };
+  register(userDetails);
 });
 
 const register = async (user) => {
-    try {
-        const res = await fetch(`${baseurl}users/register`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(user),
-        });
-        let data = await res.json();
-        if (data.error) {
-            await Toast.fire({
-                icon: "error",
-                title: data.error,
-            });
-            // window.location.reload();
-            console.log(data.err);
-        } else {
-            await Toast.fire({
-                icon: "success",
-                title: "User created successfully",
-            });
-            console.log("User created successfully");
-            // window.location.href = "./login.html";
-        }
-    } catch (error) {
-        Swal.fire({
-            title: "Error!",
-            text: "Bad request 404",
-            icon: "error",
-            confirmButtonText: "Retry",
-        });
+  try {
+    const res = await fetch(`${baseurl}users/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+    let data = await res.json();
+    if (data.error) {
+      await Toast.fire({
+        icon: "error",
+        title: data.error,
+      });
+    } else {
+      await Toast.fire({
+        icon: "success",
+        title: "User created successfully",
+      });
     }
+  } catch (error) {
+    Swal.fire({
+      title: "Error!",
+      text: "Bad request 404",
+      icon: "error",
+      confirmButtonText: "Retry",
+    });
+  }
 };
 
 // login
 
 const loginForm = document.getElementById("loginForm");
 loginForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const loginEmail = document.getElementById("loginEmail");
-    const loginPassword = document.getElementById("loginPassword");
-    const userDetails = {
-        email: loginEmail.value,
-        password: loginPassword.value,
-    };
-    console.log(userDetails);
-    login(userDetails);
+  e.preventDefault();
+  const loginEmail = document.getElementById("loginEmail");
+  const loginPassword = document.getElementById("loginPassword");
+  const userDetails = {
+    email: loginEmail.value,
+    password: loginPassword.value,
+  };
+  login(userDetails);
 });
 
 const login = async (user) => {
-    try {
-        const res = await fetch(`${baseurl}users/login`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
+  try {
+    const res = await fetch(`${baseurl}users/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+
+    let data = await res.json();
+    if (data.message) {
+      const { value: OTP } = await Swal.fire({
+        title: "Check your OTP in mailbox",
+        input: "number",
+        inputLabel: "",
+        inputPlaceholder: "Enter your OTP",
+        inputAttributes: {
+          maxlength: 4,
+          autocapitalize: "off",
+          autocorrect: "off",
+        },
+      });
+
+      if (OTP) {
+        if (data.otp == OTP) {
+          const Toast = Swal.mixin({
+            toast: true,
+            position: "top-end",
+            showConfirmButton: false,
+            timer: 1000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+              toast.addEventListener("mouseenter", Swal.stopTimer);
+              toast.addEventListener("mouseleave", Swal.resumeTimer);
             },
-            body: JSON.stringify(user),
-        });
-
-        let data = await res.json();
-        if (data.message) {
-            const { value: OTP } = await Swal.fire({
-                title: "Check your OTP in mailbox",
-                input: "number",
-                inputLabel: "",
-                inputPlaceholder: "Enter your OTP",
-                inputAttributes: {
-                    maxlength: 4,
-                    autocapitalize: "off",
-                    autocorrect: "off",
-                },
-            });
-
-            if (OTP) {
-                if (data.otp == OTP) {
-                    const Toast = Swal.mixin({
-                        toast: true,
-                        position: "top-end",
-                        showConfirmButton: false,
-                        timer: 1000,
-                        timerProgressBar: true,
-                        didOpen: (toast) => {
-                            toast.addEventListener(
-                                "mouseenter",
-                                Swal.stopTimer
-                            );
-                            toast.addEventListener(
-                                "mouseleave",
-                                Swal.resumeTimer
-                            );
-                        },
-                    });
-                    await Toast.fire({
-                        icon: "success",
-                        title: "Signed in successfully",
-                    });
-                    localStorage.setItem("userdata", JSON.stringify(data));
-                    // relocate to home page
-                    window.location.href = "../index.html";
-                } else {
-                    Swal.fire(`Invalid OTP: ${OTP}`);
-                }
-            } else {
-                Swal.fire(`Invalid OTP: ${OTP}`);
-            }
+          });
+          await Toast.fire({
+            icon: "success",
+            title: "Signed in successfully",
+          });
+          localStorage.setItem("userdata", JSON.stringify(data));
+          // relocate to home page
+          window.location.href = "../index.html";
         } else {
-            Swal.fire({
-                title: "Error!",
-                text: data.error,
-                icon: "error",
-                confirmButtonText: "Retry",
-            });
+          Swal.fire(`Invalid OTP: ${OTP}`);
         }
-    } catch (error) {
-        Swal.fire({
-            title: "Error!",
-            text: "Bad request 404",
-            icon: "error",
-            confirmButtonText: "Retry",
-        });
+      } else {
+        Swal.fire(`Invalid OTP: ${OTP}`);
+      }
+    } else {
+      Swal.fire({
+        title: "Error!",
+        text: data.error,
+        icon: "error",
+        confirmButtonText: "Retry",
+      });
     }
+  } catch (error) {
+    Swal.fire({
+      title: "Error!",
+      text: "Bad request 404",
+      icon: "error",
+      confirmButtonText: "Retry",
+    });
+  }
 };
